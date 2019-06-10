83fdbf27f561b7d35950813dd3c2e822cb00b694
CN0075 <cn0075@CN0075.lan>
2019-06-10 14:17:27 +0100

    not happy with the css

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+// add event listener for when a specific key is pressed(keydown)
+
 window.addEventListener('keydown', event => {
-    
+
+    // function ... when the query selector matches the id of the specified event key the audio that is assigned to that key 
+    // will play.
+
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
-   
-    if(!audio) return;
-    
+
+    //  if a key that is not assigned with audio is pressed nothing will happen.
+
+    if (!audio) return;
+
     audio.play()
-  })
+})
